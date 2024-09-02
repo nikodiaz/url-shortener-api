ccbc0d91227d12c545a9f5c52850d553ecebcaf5
Nicolas Diaz <nikodiaz56@gmail.com>
2024-09-02 17:31:45 -0300

    feat: Initial config

---
+++ index.js
@@ -0,0 +1,23 @@
+import express from "express"
+import dotenv from "dotenv"
+import connectDB from "./config/db.js"
+import router from "./routes/urlRoutes.js"
+
+dotenv.config()
+
+const app = express()
+const port = process.env.PORT || 3000
+
+//Connect to DB
+connectDB()
+
+//Middleware for handling JSON
+app.use(express.json())
+
+//Routes
+app.use("/", router)
+
+//Starts the server
+app.listen(port, () => {
+  console.log(`Servidor corriendo en http://localhost:${port}`)
+})
