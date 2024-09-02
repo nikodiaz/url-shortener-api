bedfa06aea0463e622c1be07ddafd99f66d53d03
Nicolas Diaz <nikodiaz56@gmail.com>
2024-09-02 17:32:26 -0300

    feat: add connection to MongoDB

---
+++ config/db.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+//Create a connection with MongoDB
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI)
+    console.log('Conectado a MongoDB')
+  } catch (error) {
+    console.error('Error al conectar a MongoDB:', error.message)
+    process.exit(1)
+  }
+}
+
+export default connectDB
