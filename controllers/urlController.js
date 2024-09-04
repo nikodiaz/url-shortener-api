5830d3c252f31141ddb75e48cddecb6f1c438ea8
Nicolas Diaz <nikodiaz56@gmail.com>
2024-09-04 18:26:52 -0300

    fix(shortenUrl): fix that a new shortened url is generated for the same original url

--- controllers/urlController.js
+++ controllers/urlController.js
@@ -6,11 +6,21 @@ const BASE_URL = process.env.BASE_URL || `http://localhost:${process.env.PORT ||
 //Controller that handler the bussines logic
 export const shortenUrl = async (req, res) => {
   const { originalUrl } = req.body
-  const shortUrl = nanoid(8)
+
+  if (!originalUrl) {
+    return res.status(400).json({ message: "La URL original es requerida." });
+  }
+
 
   try {
+    let url = await Url.findOne({ originalUrl });
+    if (url) {
+      //If it already exists, returns the existing shortened URL
+      return res.json({ shortUrl: `${BASE_URL}/${url.shortUrl}` });
+    }
+    const shortUrl = nanoid(8)
     const newUrl = new Url({ originalUrl, shortUrl })
-    newUrl.save()
+    await newUrl.save()
     res.json({ shortUrl: `${BASE_URL}/${shortUrl}` })
   } catch (e) {
     res.status(500).json({ message: `Error al acortar la URL:`, e })
