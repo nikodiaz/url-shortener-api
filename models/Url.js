3c3a2cbdc3a90215a0a47754db2fff3e336809ea
Nicolas Diaz <nikodiaz56@gmail.com>
2024-09-12 18:47:50 -0300

    feat: update url schema to add analytics

--- models/Url.js
+++ models/Url.js
@@ -1,12 +1,18 @@
 import mongoose from "mongoose";
 
+const visitsByDateSchema = new mongoose.Schema({
+  date: { type: Date, required: true },
+  count: { type: Number, default: 0 }
+})
+
 // Create a model of Url
 const urlSchema = new mongoose.Schema({
   originalUrl: { type: String, required: true },
   shortUrl: { type: String, required: true, unique: true },
   qrCode: { type: String, required: true },
-  createdBy: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: false },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: false },
   visits: { type: Number, default: 0 },
+  visitsByDate: [visitsByDateSchema],
   createdAt: { type: Date, default: Date.now }
 })
 
